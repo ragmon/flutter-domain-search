0d6a889e84d16996d53f04b48b1238f614a54b4c
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-05-13 15:49:28 +0300

    Inital project version

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Domain Search',
+//      home: HomePage(),
+    );
+  }
+}
