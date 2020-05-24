1c0a03c8c49f39f6d8d3c18e8863a7557fbe2aa6
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-05-24 18:41:00 +0300

    added i10n support

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:domainsearch/i10n/app_localizations.dart';
 import 'package:domainsearch/ui/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(App());
 
@@ -7,7 +9,18 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Domain Search',
+      onGenerateTitle: (BuildContext context) => AppLocalizations.of(context).title,
+      localizationsDelegates: [
+        const AppLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale("en"),
+        const Locale("ru"),
+        const Locale("uk"),
+      ],
+      title: AppLocalizations.of(context).title,
       theme: _buildTheme(),
       home: HomeScreen(),
     );
