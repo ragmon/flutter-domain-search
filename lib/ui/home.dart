bad6b55b78e6d529f450eb1a363451d913e35993
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-05-26 21:37:43 +0300

    added route animation

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -114,10 +114,21 @@ class _SearchingFormState extends State<SearchingForm> {
     if (_searchFieldController.text.isNotEmpty) {
       Navigator.push(
         context,
-        MaterialPageRoute(
-          builder: (_) => DomainDetailsScreen(
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => DomainDetailsScreen(
             domain: _searchFieldController.text,
           ),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            var begin = Offset(0.0, 1.0);
+            var end = Offset.zero;
+            var tween = Tween(begin: begin, end: end);
+            var offsetAnimation = animation.drive(tween);
+
+            return SlideTransition(
+              position: offsetAnimation,
+              child: child,
+            );
+          },
         ),
       );
     }
